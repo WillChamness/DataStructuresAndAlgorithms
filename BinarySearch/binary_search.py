3597fa585837a8186dfd8713414469eeb52cdf7d
WillChamness <wzchamness@gmail.com>
2022-08-11 15:31:43 -0400

    Added docstrings

---
+++ BinarySearch/binary_search.py
@@ -0,0 +1,79 @@
+def binary_search(sorted_list, target, choice="recursive"):
+    """
+    Searches a sorted list of size n.
+    Time complexity: O(log(n))
+    Space complexity: O(1)
+
+    Strategy:
+    Compare the target with the item in the middle of the array. If the target
+    is equal to the item, the target has been found. If the target is less than 
+    the item, the target is in the left half. Otherwise, the target is in the
+    right half. Continue to divide and conquer until either the target has
+    been found or there is nothing left to search.
+
+
+    Example:
+    Given [0, 1, 2, 3, 4]. Search the list for the number 3.
+    
+    Begin at the middle index (index 2). 3 > 2, so if 3 is in the list, it will be to 
+    the right of 2. Now consider the sublist containing all items to the right
+    of 2: [3. 4]. 
+    
+    Begin at the middle index (index 0.5). We will arbitrarily round down since there is
+    an even number of items. 3 == 3, so we have found the target. 
+    """
+
+    if choice == "recursive":
+        return __binary_search_rec__(sorted_list, target, 0, len(sorted_list) - 1)
+    else:
+        return __binary_search__(sorted_list, target)
+
+
+def __binary_search__(l, target):
+    lo = 0
+    hi = len(l) - 1
+
+    while not (lo > hi): # continue until there is nothing left to search
+        mid = (lo + hi) // 2
+        if l[mid] == target: # target found, return its index
+            return mid
+        elif target < l[mid]: # target is in the left half of the sublist, update pointers
+            hi = mid - 1
+        else: # target is in the right half of the sublist, update pointers
+            lo = mid + 1 
+    
+    return -1 # target not found
+
+
+def __binary_search_rec__(l, target, lo, hi):
+    if lo > hi: # continue until there is nothing left to search
+        return -1
+    
+    mid = (lo + hi) // 2
+    if l[mid] == target: # target found, return its index
+        return mid
+    elif target < l[mid]: # target is in the left half, perform binary search on the left half of the sublist
+        return __binary_search_rec__(l, target, lo, mid - 1)
+    else:
+        return __binary_search_rec__(l, target, mid + 1, hi) # target is in the right half, perform binary search on the right half of the sublist
+    
+
+        
+
+def main():
+    list1 = [1,2,3,4,5,6,7,8,9]
+    list2 = [1,2,3,4,5,6,7,8,9,10]
+    print(binary_search(list1, 2)) # index 1
+    print(binary_search(list1, 10)) # not found; -1
+    print(binary_search(list2, 10)) # 9
+    print(binary_search(list2, 1)) # 0
+    print(binary_search([], 1000)) # not found; -1
+    print(binary_search(list1, 3, "a")) # 2
+    print(binary_search(list1, 9, "a")) # 8
+    print(binary_search(list2, 3, "a")) # 2
+    print(binary_search(list2, 9, "a")) # 8
+    print(binary_search([], 10, "a")) # not found; -1
+
+
+if __name__ == "__main__":
+    main()
---
+++ LinearSearch/linear_search.py
@@ -0,0 +1,60 @@
+def linear_search(li, target, choice="recursive"):
+    """ 
+    Searches a list of size n.
+    Time complexity: O(n)
+    Space complexity: O(n)
+
+    Strategy:
+    Starting at index 0, compare the item to the target. If they are equal,
+    the target has been found. Otherwise, check the next item in the list.
+    Continue until either the target has been found or you have reached
+    the end of the list.
+
+
+    Example:
+    Given [2, 4, 0, 3, 1]. Search for the number 3.
+
+    Start at the beginning. 3 != 2, so check the next item. 3 != 4, so
+    check the next item. 3 != 0, so check the next item. 3 == 3, so
+    the target has been found.
+    """
+
+    if choice == "recursive":
+        return __linear_search_rec__(li, target, 0)
+    else:
+        return __linear_search__(li, target)
+
+
+def __linear_search__(li, target):
+    # iterate through the list until the target has been found
+    for i in range(len(li)): 
+        if li[i] == target:
+            return i
+    return -1 # target not found
+
+
+def __linear_search_rec__(li, target, index):
+    if index < 0 or len(li) <= index: # continue until you are out of bounds
+        return -1
+    if li[index] == target: # target found, return its index
+        return index
+    else: # target not found, search the next item in the list
+        return __linear_search_rec__(li, target, index + 1)
+
+
+def main():
+    l1 = [1,2,3,4,5,6,7,8,9]
+    l2 = [1,2,3,4,5,6,7,8,9,10]
+    print(linear_search(l2, 5)) # index 4
+    print(linear_search([], 1000)) # not found; -1
+    print(linear_search(l2, 1000)) # not found; -1
+    print(linear_search(l1, 7, "a")) # index 6
+    print(linear_search(l2, 5, "a")) # index 4
+    print(linear_search(l2, 1000, "a")) # not found; -1
+    print(linear_search([], 10000, "a")) # not found; -1
+
+
+
+
+if __name__ == "__main__":
+    main()
