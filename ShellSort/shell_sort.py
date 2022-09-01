dfb71054d138f24ae49335f71c3b56162b0bc025
WillChamness <wzchamness@gmail.com>
2022-08-31 23:14:50 -0400

    fix typo

--- ShellSort/shell_sort.py
+++ ShellSort/shell_sort.py
@@ -10,7 +10,7 @@ def shell_sort(li):
     closer the right before performing insertion sort. 
 
     Strategy:
-    Let gap == m_1 > 0. Partiion the list such that all items' indeces are congruent to each 
+    Let gap == m_1 > 0. Partition the list such that all items' indeces are congruent to each 
     other (mod m_1). Then perform insertion sort on the sublists. Put the items into the 
     original list in order such that the set of their new indeces is still the same as the set
     of their indeces before partitioning them. Then perform this process for gap == m_(k+1) < m_k 
