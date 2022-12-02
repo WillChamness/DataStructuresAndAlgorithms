18ee33ead35c09920fd06f0b75e27afd1a7cc099
WillChamness <wzchamness@gmail.com>
2022-12-02 14:01:11 -0500

    add bubble_sort.py

---
+++ BubbleSort/bubble_sort.py
@@ -0,0 +1,48 @@
+def bubble_sort(li, choice="recursive"):
+    if choice == "recursive":
+        _bubble_sort_rec(li, 0, len(li) - 1)
+    else:
+        _bubble_sort(li)
+
+def _bubble_sort(li):
+    for i in range(len(li) - 1, -1, -1): # consider the sublist from 0 to i (stop at i-1 because you will access i-1 + 1 later)
+        for j in range(i): # go until you reach the end of the sublist
+            if li[j] > li[j+1]: # if the current item is greater than the next, swap the two
+                _swap(li, j, j+1)
+
+
+def _bubble_sort_rec(li, current_index, last_index):
+    if last_index <= 0: # if you have reached the trivial case, you are done
+        return
+    if current_index >= last_index: # if you have reached the end of the sublist, continue to the next sublist
+        _bubble_sort_rec(li, 0, last_index - 1)
+        return
+    
+    if li[current_index] > li[current_index + 1]: # swap if the current item is greater than the next
+        _swap(li, current_index, current_index + 1)
+
+    _bubble_sort_rec(li, current_index + 1, last_index) # regardless of if you swapped, move to the next item
+
+
+def _swap(li, i1, i2):
+    temp = li[i1]
+    li[i1] = li[i2]
+    li[i2] = temp
+
+
+def main():
+    import random as r
+    l = []
+    for i in range(10): l.append(r.randint(0, 100))
+    print(f"Before: {l}")
+    bubble_sort(l, "a")
+    print(f"After: {l}")
+
+    l = []
+    for i in range(10): l.append(r.randint(0, 100))
+    print(f"\nBefore: {l}")
+    bubble_sort(l)
+    print(f"After: {l}")
+
+if __name__ == "__main__":
+    main()
