96687768c727b0d33794f6b122e0021bd56649b8
WillChamness <wzchamness@gmail.com>
2022-12-15 23:54:22 -0500

    update bst.py
    add _insert()

--- BinarySeachTree/bst.py
+++ BinarySeachTree/bst.py
@@ -15,3 +15,15 @@ class BST:
             self.root = self.TreeNode(item)
         else:
             self._insert(item, self.root)
+
+    def _insert(self, item, current_node):
+        if item < current_node.item:
+            if current_node.left is None:
+                current_node.left = self.TreeNode(item)
+            else:
+                self._insert(item, current_node.left)
+        elif item > current_node.item:
+            if current_node.right is None:
+                current_node.right = self.TreeNode(item)
+            else:
+                self._insert(item, current_node.right)
