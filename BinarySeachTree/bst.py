ea5dd0bdc7e3f9891391a897e3ee640268ba4645
WillChamness <wzchamness@gmail.com>
2022-12-27 20:57:37 -0500

    update bst.py
    add _remove()

--- BinarySeachTree/bst.py
+++ BinarySeachTree/bst.py
@@ -33,3 +33,26 @@ class BST:
             return None
         else:
             return self._remove(item, self.root)
+
+    def _remove(self, item, current_node):
+        if item < current_node.item:
+            if current_node.left is not None:
+                self._remove(item, current_node.left)
+        elif item > current_node.item:
+            if current_node.right is not None:
+                self._remove(item, current_node.right)
+        else: 
+            if current_node.left is not None and current_node.right is not None: # case where node has two children
+                # Go right then all the way left. You could also go left then all the way right instead. 
+                # This will guarentee an item less than the current node
+                pointer = current_node.right
+                while pointer.left is not None:
+                    pointer = pointer.left
+                current_node.item = pointer.item # change node's value to the pointer's value
+                pointer = None # remove the pointer
+            elif current_node.left is not None and current_node.right is None: # case where node has one child
+                current_node = current_node.left
+            elif current_node.right is not None and current_node.left is None: # case where node has one child
+                current_node = current_node.right
+            else: # case where node has no children
+                current_node = None
