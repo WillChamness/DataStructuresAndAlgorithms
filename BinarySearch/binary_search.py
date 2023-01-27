d9e4d3ed9d50e26dc4913731500e263dece6c0ea
WillChamness <wzchamness@gmail.com>
2023-01-27 17:09:30 -0500

    update binary_search.py
    fix multiple typos

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,4 +1,4 @@
-def binary_search(sorted_list, target, recurisve=True):
+def binary_search(sorted_list, target, recursive=True):
     """
     Searches a sorted list of size n.
     Time complexity: O(log(n))
@@ -18,65 +18,65 @@ def binary_search(sorted_list, target, recurisve=True):
 
     Example:
     Given [0, 1, 2, 3, 4]. Search the list for the number 3.
-    
+
     Begin at the middle index (index 2). 3 > 2, so if 3 is in the list, it will be to 
     the right of 2. Now consider the sublist containing all items to the right
     of 2: [3. 4]. 
-    
+
     Begin at the middle index (index 0.5). We will arbitrarily round down since the middle
     index is not an integer. 3 == 3, so we have found the target. 
     """
 
-    if recurisve:
-        return _binray_search_rec(sorted_list, target, 0, len(sorted_list) - 1)
+    if recursive:
+        return _binary_search_rec(sorted_list, target, 0, len(sorted_list) - 1)
     else:
-        return _binray_search(sorted_list, target)
+        return _binary_search(sorted_list, target)
 
 
-def _binray_search(l, target):
+def _binary_search(l, target):
     lo = 0
     hi = len(l) - 1
 
-    while not (lo > hi): # continue until there is nothing left to search
+    while not (lo > hi):  # continue until there is nothing left to search
         mid = (lo + hi) // 2
-        if l[mid] == target: # target found, return its index
+        if l[mid] == target:  # target found, return its index
             return mid
-        elif target < l[mid]: # target is in the left half of the sublist, update pointers
+        elif target < l[mid]:  # target is in the left half of the sublist, update pointers
             hi = mid - 1
-        else: # target is in the right half of the sublist, update pointers
-            lo = mid + 1 
-    
-    return -1 # target not found
+        else:  # target is in the right half of the sublist, update pointers
+            lo = mid + 1
+
+    return -1  # target not found
 
 
-def _binray_search_rec(l, target, lo, hi):
-    if lo > hi: # continue until there is nothing left to search
+def _binary_search_rec(l, target, lo, hi):
+    if lo > hi:  # continue until there is nothing left to search
         return -1
-    
-    mid = (lo + hi) // 2
-    if l[mid] == target: # target found, return its index
-        return mid
-    elif target < l[mid]: # target is in the left half, perform binary search on the left half of the sublist
-        return _binray_search_rec(l, target, lo, mid - 1)
-    else:
-        return _binray_search_rec(l, target, mid + 1, hi) # target is in the right half, perform binary search on the right half of the sublist
-    
 
-        
+    mid = (lo + hi) // 2
+    if l[mid] == target:  # target found, return its index
+        return mid
+    # target is in the left half, perform binary search on the left half of the sublist
+    elif target < l[mid]:
+        return _binary_search_rec(l, target, lo, mid - 1)
+    else:
+        # target is in the right half, perform binary search on the right half of the sublist
+        return _binary_search_rec(l, target, mid + 1, hi)
+
 
 def main():
-    list1 = [1,2,3,4,5,6,7,8,9]
-    list2 = [1,2,3,4,5,6,7,8,9,10]
-    print(binary_search(list1, 2)) # index 1
-    print(binary_search(list1, 10)) # not found; -1
-    print(binary_search(list2, 10)) # 9
-    print(binary_search(list2, 1)) # 0
-    print(binary_search([], 1000)) # not found; -1
-    print(binary_search(list1, 3, False)) # 2
-    print(binary_search(list1, 9, False)) # 8
-    print(binary_search(list2, 3, False)) # 2
-    print(binary_search(list2, 9, False)) # 8
-    print(binary_search([], 10, False)) # not found; -1
+    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(binary_search(list1, 2))  # index 1
+    print(binary_search(list1, 10))  # not found; -1
+    print(binary_search(list2, 10))  # 9
+    print(binary_search(list2, 1))  # 0
+    print(binary_search([], 1000))  # not found; -1
+    print(binary_search(list1, 3, False))  # 2
+    print(binary_search(list1, 9, False))  # 8
+    print(binary_search(list2, 3, False))  # 2
+    print(binary_search(list2, 9, False))  # 8
+    print(binary_search([], 10, False))  # not found; -1
 
 
 if __name__ == "__main__":
