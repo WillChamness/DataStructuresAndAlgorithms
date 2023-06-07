caede9e38bb4bac85e1559709bec0938fd656f10
WillChamness <wzchamness@gmail.com>
2023-06-07 14:26:23 -0400

    update min_heap.py
    add static method heapify()

--- Heap/min_heap.py
+++ Heap/min_heap.py
@@ -67,3 +67,23 @@ class MinHeap(Heap):
         elif min_index == self._right_child_index(index): # case where right child is smallest
             swap(self.heap, index, self._right_child_index(index))
             self._percolate_down(self._right_child_index(index))
+
+
+    @staticmethod
+    def heapify(li):
+        def swap(li, i1, i2):
+            temp = li[i1]
+            li[i1] = li[i2]
+            li[i2] = temp
+        def percolate_up(li, index):
+            parent_index = (index - 1) // 2
+
+            if parent_index < 0: return # has no parent
+            if li[index] > li[parent_index]: return # parent is smaller
+            swap(li, index, parent_index)
+            percolate_up(li, parent_index)
+        
+        for n in range(len(li)):
+            percolate_up(li, n)
+        
+        return li
