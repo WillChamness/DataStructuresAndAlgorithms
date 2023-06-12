e80775dbcb2f4843b2c120e147205a7ec841cba8
WillChamness <wzchamness@gmail.com>
2023-06-11 20:00:59 -0400

    add max_heap.py

---
+++ Heap/max_heap.py
@@ -0,0 +1,15 @@
+from heap import Heap
+
+
+class MaxHeap(Heap):
+    def __init__(self, capacity=10, initial_heap=None):
+        Heap.__init__(self, capacity, initial_heap)
+
+    
+    def insert(self, item):
+        if self.is_full():
+            raise Exception("MaxHeap is full.")
+
+        self.heap[self._next_empty_slot()] = item
+        self._percolate_up(self._next_empty_slot())
+        self.size += 1
