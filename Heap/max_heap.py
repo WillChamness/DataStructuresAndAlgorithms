0721c2c9224673f05aef497a2ae68bcab32f7fe0
WillChamness <wzchamness@gmail.com>
2023-06-13 10:44:16 -0400

    update max_heap.py
    add _percolate_up()

--- Heap/max_heap.py
+++ Heap/max_heap.py
@@ -13,3 +13,18 @@ class MaxHeap(Heap):
         self.heap[self._next_empty_slot()] = item
         self._percolate_up(self._next_empty_slot())
         self.size += 1
+
+
+    def _percolate_up(self, index):
+        def swap(li, i1, i2):
+            temp = li[i1]
+            li[i1] = li[i2]
+            li[i2] = temp
+
+        if not self._has_parent(index): # do nothing if you reached the top
+            return
+        if self.heap[index] < self.heap[self._parent_index(index)]: # do nothing if parent is greater than child
+            return
+        
+        swap(self.heap, index, self._parent_index(index))
+        self._percolate_up(self._parent_index(index))
