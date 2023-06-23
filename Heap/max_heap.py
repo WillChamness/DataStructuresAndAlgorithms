319d180a614821ea86f7a2975eaf1beac9cfa22e
WillChamness <wzchamness@gmail.com>
2023-06-23 11:20:13 -0400

    update max_heap.py
    add remove_max() and _percolate_down()

--- Heap/max_heap.py
+++ Heap/max_heap.py
@@ -28,3 +28,42 @@ class MaxHeap(Heap):
         
         swap(self.heap, index, self._parent_index(index))
         self._percolate_up(self._parent_index(index))
+
+
+    def remove_max(self):
+        if self.is_empty():
+            raise Exception("MaxHeap is empty.")
+        
+        item = self.heap[0]
+        self.heap[0] = self.heap[self._next_empty_slot() - 1]
+        self.size -= 1
+        self._percolate_down(0)
+        return item
+
+
+    def _percolate_down(self, index):
+        def swap(li, i1, i2):
+            temp = li[i1]
+            li[i1] = li[i2]
+            li[i2] = temp
+        def max(li, i1, i2):
+            if li[i1] > li[i2]:
+                return i1
+            else:
+                return i2
+
+        if not self._has_left_child(index):
+            return
+        if not self._has_right_child(index):
+            if index != max(self.heap, index, self._left_child_index(index)):
+                swap(self.heap, index, self._left_child_index(index))
+            return
+        
+        max_index = max(self.heap, index, max(self.heap, self._left_child_index(index), 
+            self._right_child_index(index)))
+        if max_index == self._left_child_index(index):
+            swap(self.heap, index, self._left_child_index(index))
+            self._percolate_down(self._left_child_index(index))
+        elif max_index == self._right_child_index(index):
+            swap(self.heap, index, self._right_child_index(index))
+            self._percolate_down(self._right_child_index(index))
